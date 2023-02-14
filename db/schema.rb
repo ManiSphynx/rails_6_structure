86ff7979307ca608ae5449734c97914b87004df9
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-14 11:36:08 -0600

    feat: add articles with user association, model, controller and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_14_162356) do
-
+ActiveRecord::Schema.define(version: 2023_02_14_171703) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +26,10 @@ ActiveRecord::Schema.define(version: 2023_02_14_162356) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 end
