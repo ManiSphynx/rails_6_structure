0571ecb6637880f4bc27d869a49a4ad2da570e9b
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-16 13:36:13 -0600

    feat: add global helpers to have them in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  include ApplicationHelper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # Returns true if a user is currently authenticated, false otherwise
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    unless logged_in?
+      flash_message("You must be logged in to perform that action", :alert)
+      redirect_to login_path
+    end
+  end
 end
