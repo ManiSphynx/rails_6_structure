b8e941ce0937a73314c9e3e6f6db82bf85bbe5ed
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-21 11:53:41 -0600

    chore: adding validations to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: {
+              minimum: 3,
+              maximum: 25
+            }
 end
