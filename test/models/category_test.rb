6548ab3a01fab2564b218765016a900513f67c1c
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-21 10:15:53 -0600

    test: adding basic test to category controller

---
+++ test/models/category_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+  test "category should be valid" do
+    @category = Category.new(name: "sports")
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category = Category.new(name: "")
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+  end
+
+  test "name should not be too long" do
+  end
+
+  test "name should not be too short" do
+  end
+end
